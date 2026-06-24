3ae07b0dfc3199fc16225b6efbe8c7886096aa31
kayque001-cyber <okayque363@gmail.com>
2026-06-23 21:41:57 -0300

    atualização/sistema

--- Sistema_Atac Woest.py
+++ Sistema_Atac Woest.py
@@ -15,6 +15,14 @@ def salvar_funcionarios():
 funcionarios = carregar_funcionarios()
 
 
+def encontrar_funcionario(nome):
+    for funcionario in funcionarios:
+        if nome == funcionario["nome"]:
+            return funcionario
+    return None
+
+
+
 
 # Imprimir os funcionarios. dicionario.
 def listar_funcionarios():
@@ -31,16 +39,14 @@ def listar_funcionarios():
 def buscar_funcionario():
     nome_buscado = input("Digite o nome do funcionario para buscar: ")
 
-
-
-    for funcionario in funcionarios:
-        if nome_buscado == funcionario['nome']:
+    funcionario = encontrar_funcionario(nome_buscado)
+    if funcionario:
             print(f"Funcionario encontrado")
             print(f"ID: {funcionario['id']}")
             print(f"Nome: {funcionario['nome']}")
             print(f"Idade: {funcionario['idade']}")
             print(f"Cargo: {funcionario['cargo']}")
-            break
+
     else:
             print("Funcionario não encontrado")
 
@@ -51,9 +57,9 @@ def adicionar_funcionario():
     novo_nome = input("Digite o nome do novo funcionario: ")
     nova_idade = int(input("Digite a idade do novo funcionario: "))
     novo_cargo = input("Digite o cargo do novo funcionario: ")
-
+    novo_id = max(funcionarios["id"] for funciocionario in funcionarios) + 1
     novo_funcionario = {
-        "id": len(funcionarios) + 1,
+        "id": novo_id,
         "nome": novo_nome,
         "idade": nova_idade,
         "cargo": novo_cargo
@@ -71,34 +77,32 @@ def adicionar_funcionario():
 def atualizar_funcionario():
     nome_atualizar = input("digite o nome do funcionario para atualizar: ")
 
-    for funcionario in funcionarios:
-        if nome_atualizar == funcionario['nome']:
-            novo_cargo = input("Digite o novo cargo do funcionario: ")
-            funcionario['cargo'] = novo_cargo
+    funcionario = encontrar_funcionario(nome_atualizar)
+    if funcionario:
+        novo_cargo = input("Digite o novo cargo do funcionario: ")
+        funcionario['cargo'] = novo_cargo
 
-            salvar_funcionarios()
+        salvar_funcionarios()
 
-            print()
-            print("Cargo atualizado com sucesso!")
-            break
+        print()
+        print("Cargo atualizado com sucesso!")
+
     else:
         print("Funcionario não encontrado")
 
 def remover_funcionario():
     nome_remover = input ("Digite o nome do funcionario para remover: ")
 
-    for funcionario in funcionarios:
-        if nome_remover == funcionario['nome']:
+    funcionario = encontrar_funcionario(nome_remover)
+    if funcionario:
             print("Funcionario encontrado")
             funcionarios.remove(funcionario)
 
             salvar_funcionarios()
-
-            print()
-            print("Removendo funcionario...")
+            
             print()
             print("Funcionario removido com sucesso!")
-            break
+
     else:
             print("Funcionario não encontrado")
 
---
+++ texte.py
@@ -0,0 +1,15 @@
+funcionarios = [
+    {
+        "Nome": "Kayque"
+    },
+    {
+        "Nome": "Carlos"
+    }
+]
+
+
+
+def buscar_funcionario(nome):
+    print(f"Olá {nome}")
+
+buscar_funcionario("Kayque")
